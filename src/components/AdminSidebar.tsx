e48014000333658b67c6c1630070d3a20ab1cd54
manish50kumar <manish5060773@gmail.com>
2024-02-14 23:09:43 +0530

    Write side bar component

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,6 +1,73 @@
+import { IconType } from "react-icons";
+import { Link,  useLocation } from "react-router-dom";
+import { RiDashboardFill, RiShoppingBag3Fill, } from "react-icons/ri";
+import { AiFillFileText } from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
+
 const AdminSidebar = () => {
+
+  // const navigate = useNavigate();
+  const location = useLocation();
+
   return (
-    <div>Admin Sidebar</div>
+    <aside>
+      <h2>logo.</h2>
+      <div>
+        <h5>Dashboard</h5>
+        <ul>
+          {/* <Li
+            url="/admin/dashboard"
+            text="Dashboard"
+            Icon={RiDashboardFill}
+            location={location}
+          /> */}
+          <li>
+            <Link to={"/admin/dashboard"}>
+              <RiDashboardFill/>
+              Dashboard               
+              </Link >
+        </li>
+          <li>
+            <Link to={"/admin/product"}>
+              <RiShoppingBag3Fill/>
+              Product               
+              </Link >
+        </li>
+          <li>
+            <Link to={"/admin/customer"}>
+              <IoIosPeople/>
+              Customer               
+              </Link >
+        </li>
+          <li>
+            <Link to={"/admin/transaction"}>
+              <AiFillFileText/>
+              Transaction               
+              </Link >
+        </li>
+          
+
+          
+
+        </ul>
+      </div>
+    </aside>
   )
-}
+};
+
+// interface LiProps {
+//   url: string,
+//   text: string,
+//   location: Location,
+//   Icon: IconType
+// }
+// const Li = ({url,location,Icon,text}:LiProps) => (
+//   <li>
+//     <Link to={url}>
+//       <Icon/>
+//       {text}
+//     </Link>
+//   </li>
+// )
+
 export default AdminSidebar
